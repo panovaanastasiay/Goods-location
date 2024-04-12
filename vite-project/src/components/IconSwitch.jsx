0a93f0fd5cf2d0e65ccf1d5497d970806a9bd54e
Anastasia <anastasiapan93@gmail.com>
2024-04-11 19:47:49 -0700

    added

---
+++ vite-project/src/components/IconSwitch.jsx
@@ -0,0 +1,11 @@
+
+
+const IconSwitch = ({ icon, onSwitch }) => {
+    return (
+      <div className="icon-switch" onClick={onSwitch}>
+        <i className="material-icons">{icon}</i>
+      </div>
+    );
+  };
+  
+  export default IconSwitch;
